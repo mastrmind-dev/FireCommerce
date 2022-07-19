bd0d68192df8e2b833c65ab9ac90a12279d315d1
mastrmind-dev <asapthaka@gmail.com>
2022-07-19 21:02:23 +0530

    created two folders and include relavant files inside of them -> cliend and server

---
+++ client/src/pages/ProductInfo.js
@@ -0,0 +1,53 @@
+import { doc, getDoc } from "@firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import Layout from "../components/Layout";
+import fireDB from "../fireConfig";
+
+const ProductInfo = () => {
+  const [product, setProduct] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const params = useParams();
+
+  useEffect(() => {
+    getdata();
+  }, []);
+
+  const getdata = async () => {
+setLoading(true)
+    try {
+      const productTemp = await getDoc(
+        doc(fireDB, "products", params.productid)
+      );
+      console.log(productTemp.data());
+      setProduct(productTemp.data());
+      setLoading(false);
+    } catch (error) {
+      console.log("error in productinfo page, in getdata function =>");
+      console.log(error);
+      setLoading(false);
+    }
+  };
+
+  return (
+    <Layout loading={loading}>
+      <div className="container">
+        <div className="text-center">
+          <h1>{product.name}</h1>
+          <img
+            src={product.imageURL}
+            alt="image of the product"
+            className="product-info-img"
+          />
+          <hr />
+          <h4>{product.description}</h4>
+        </div>
+        <div className='d-flex justify-content-end'>
+          <button>ADD TO CART</button>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default ProductInfo;
